0923a479002e16abec3cf121155962d0cad31ecc
sridhar09 <sridhar.mundra@gmail.com>
2022-03-19 13:18:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3XS5G77IZIT65IXM"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
